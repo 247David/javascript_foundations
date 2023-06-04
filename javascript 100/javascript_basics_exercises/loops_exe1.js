b027d3ed86d2f294bf941ad658fb4980cf7881ec
David Jaiyeola <davidjaiye247@gmail.com>
2023-06-04 05:49:39 -0400

    Reorganized folders and completed take two loop exercise in JS 100 exercises.

---
+++ javascript 100/javascript_basics_exercises/loops_exe1.js
@@ -0,0 +1,38 @@
+//1. Loop and Log - Add some code inside of the for loop below that logs 
+//the current value of i to the console on each iteration. 
+//Before you run the code: What sequence of numbers do you expect to be logged?
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log(i);
+//   }
+
+  //output:
+  //0
+  //2
+  //4
+  //6
+  //8
+  //10
+
+  //2. Countdown - The code below logs the numbers from 1 to 10. 
+  //Change it, so that it instead logs the numbers from 10 to 1 in decreasing order, 
+  //and then logs 'Launch!'.
+
+//   for (let i = 10; i >= 1; i -= 1){
+//     console.log(i);
+//   }
+
+
+//3. Triple Greeting - Write a loop that logs greeting three times 
+
+// let greeting = 'Aloha!';
+
+// for (let i = 0; i < 3; i += 1) {
+//     console.log(greeting);
+// }
+
+//4. Take Two - Write a for loop that iterates over all numbers from 1 to 100, and 
+//outputs the result of multiplying each element by 2.
+
+for (let i = 1; i <= 100; i += 1) {
+    console.log(i * 2);
+}
