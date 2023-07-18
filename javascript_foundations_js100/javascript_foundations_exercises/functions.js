db5a067d31e497c641181cc7095170799799b676
David Jaiyeola <davidjaiye247@gmail.com>
2023-07-17 23:17:43 -0300

    Updated exercises in functions.js

--- javascript_foundations_js100/javascript_foundations_exercises/functions.js
+++ javascript_foundations_js100/javascript_foundations_exercises/functions.js
@@ -29,16 +29,79 @@
 // Change the function greet from the previous exercise, so that it doesn't take any arguments, 
 //and instead calls the functions greeting and recipient defined below.
 
-function greeting() {
-    return 'Good morning';
-  }
+// function greeting() {
+//     return 'Good morning';
+//   }
   
-function recipient() {
-    return 'Launch School';
-  }
+// function recipient() {
+//     return 'Launch School';
+//   }
 
-function greet() {
-    console.log(`${greeting()}, ${recipient()}!`);
-}
+// function greet() {
+//     console.log(`${greeting()}, ${recipient()}!`);
+// }
 
-greet();
+// greet();
+
+//4. Calculate BMI - Create a function that calculates a person's body mass index (BMI). 
+//It should take two parameters: someone's height (in cm) and weight (in kg). 
+//The formula for calculating the BMI is as follows
+
+// bmi = weightInKilograms / heightInMeters**2;
+
+
+// function calculateBMI (heightInCm, weightinKg) {
+//     return (weightinKg / (heightInCm * 0.01)**2).toFixed(2);
+// }
+
+// console.log(calculateBMI(180, 80));
+
+//5. Calculate Cat Age - Implement a function catAge that takes a number of human years as 
+//input and converts them into cat years. Cat years are calculated as follows:
+
+// The first human year corresponds to 15 cat years.
+// The second human year corresponds to 9 cat years.
+// Every subsequent human year corresponds to 4 cat years.
+//output:
+// catAge(0); // 0
+// catAge(1); // 15
+// catAge(2); // 24
+// catAge(3); // 28
+// catAge(4); // 32
+
+// function catAge (humanYears) {
+//     let catAge = 0; 
+//     let count = humanYears;
+//     while (count > 0) {
+
+//         if (count === 1) {
+//             catAge += 15;
+//         } else if(count === 2) {
+//             catAge += 9;
+//         } else {
+//             catAge +=4;
+//         } 
+//         count -= 1;
+//     }
+
+//     return catAge;
+// }
+
+// //alt solution:
+// function catAge(humanYears) {
+//     switch(humanYears) {
+//       case 0:
+//         return 0;
+//       case 1:
+//         return 15;
+//       case 2:
+//         return 24;
+//       default:
+//         return 24 + (humanYears - 2) * 4;
+//     }
+//   }
+// console.log(catAge(0));
+// console.log(catAge(1));
+// console.log(catAge(2));
+// console.log(catAge(3));
+// console.log(catAge(4));
